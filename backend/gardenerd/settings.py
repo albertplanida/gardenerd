0f96d4e86972451c818b59ddefbf62963af78199
Work <work@Alberts-Mini.localdomain>
2026-07-14 15:14:09 -0500

    added basic graphql schema and endpoint

--- backend/gardenerd/settings.py
+++ backend/gardenerd/settings.py
@@ -41,6 +41,7 @@ CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[])
 # Application definition
 
 INSTALLED_APPS = [
+    'strawberry.django',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
