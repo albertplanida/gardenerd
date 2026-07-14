4d425836766251600af83517220807912cfbbf5e
Work <work@Alberts-Mini.localdomain>
2026-07-14 18:18:38 -0500

    GARD-19 updated next config to rewrite /graphql/ endpoint

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/graphql",
+        destination:
+          process.env.GRAPHQL_ENDPOINT ?? "http://localhost:8000/graphql/",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
