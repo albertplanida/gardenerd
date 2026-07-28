66c9ad748a5aba87fea65cb0843cff334129bb37
Work <work@Alberts-Mini.localdomain>
2026-07-28 17:03:29 -0500

    GARD-26 added mutations, query, and type for Container model

--- backend/gardenerd/schema.py
+++ backend/gardenerd/schema.py
@@ -1,11 +1,19 @@
 import strawberry
 
+from apps.containers.graphql.Container.mutations import ContainerMutations
+from apps.containers.graphql.Container.queries import ContainerQueries
+
 
 @strawberry.type
-class Query:
+class Query(ContainerQueries):
     @strawberry.field
     def health(self) -> str:
         return 'ok'
 
 
-schema = strawberry.Schema(query=Query)
+@strawberry.type
+class Mutation(ContainerMutations):
+    pass
+
+
+schema = strawberry.Schema(query=Query, mutation=Mutation)
